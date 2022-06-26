f86754a7fb0a31e52a330b91bdbb9c641216b71d
myxroft <josiahbuhayan@gmail.com>
2022-06-27 02:03:24 +0800

    ui fix -- from demo cleanup

--- app/src/main/java/com/salvador/detectionthingy/MonitoringData.java
+++ app/src/main/java/com/salvador/detectionthingy/MonitoringData.java
@@ -194,7 +194,7 @@ public class MonitoringData extends AppCompatActivity {
         // chart.spin(2000, 0, 360);
 
         Legend l = chart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
@@ -219,66 +219,66 @@ public class MonitoringData extends AppCompatActivity {
         return Double.valueOf(totDiff.doubleValue() / 3600);
     }
 
-    private void setData(int count, float range) {
-
-        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
-        // the chart.
-        for (int i = 0; i < count ; i++) {
-            entries.add(new PieEntry((float) ((Math.random() * range) + range / 5),
-                    "Test",
-                    getResources().getDrawable(R.drawable.ic_launcher_foreground)));
-        }
-
-        PieDataSet dataSet = new PieDataSet(entries, "Time Spent");
-
-
-        dataSet.setDrawIcons(false);
-
-        dataSet.setSliceSpace(3f);
-        dataSet.setIconsOffset(new MPPointF(0, 40));
-        dataSet.setSelectionShift(5f);
-
-        // add a lot of colors
-
-        ArrayList<Integer> colors = new ArrayList<>();
-
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
-
-        colors.add(ColorTemplate.getHoloBlue());
-
-        dataSet.setColors(colors);
-        //dataSet.setSelectionShift(0f);
-
-        PieData data = new PieData(dataSet);
-        data.setValueFormatter(new PercentFormatter());
-
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.WHITE);
-//        data.setValueTypeface(tfLight);
-        chart.setData(data);
-
-        // undo all highlights
-        chart.highlightValues(null);
-
-        chart.invalidate();
-    }
+//    private void setData(int count, float range) {
+//
+//        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+//        // the chart.
+//        for (int i = 0; i < count ; i++) {
+//            entries.add(new PieEntry((float) ((Math.random() * range) + range / 5),
+//                    "Test",
+//                    getResources().getDrawable(R.drawable.ic_launcher_foreground)));
+//        }
+//
+//        PieDataSet dataSet = new PieDataSet(entries, "Time Spent");
+//
+//
+//        dataSet.setDrawIcons(false);
+//
+//        dataSet.setSliceSpace(3f);
+//        dataSet.setIconsOffset(new MPPointF(0, 40));
+//        dataSet.setSelectionShift(5f);
+//
+//        // add a lot of colors
+//
+//        ArrayList<Integer> colors = new ArrayList<>();
+//
+//        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+//            colors.add(c);
+//
+//        for (int c : ColorTemplate.JOYFUL_COLORS)
+//            colors.add(c);
+//
+//        for (int c : ColorTemplate.COLORFUL_COLORS)
+//            colors.add(c);
+//
+//        for (int c : ColorTemplate.LIBERTY_COLORS)
+//            colors.add(c);
+//
+//        for (int c : ColorTemplate.PASTEL_COLORS)
+//            colors.add(c);
+//
+//        colors.add(ColorTemplate.getHoloBlue());
+//
+//        dataSet.setColors(colors);
+//        //dataSet.setSelectionShift(0f);
+//
+//        PieData data = new PieData(dataSet);
+//        data.setValueFormatter(new PercentFormatter());
+//
+//        data.setValueTextSize(11f);
+//        data.setValueTextColor(Color.WHITE);
+////        data.setValueTypeface(tfLight);
+//        chart.setData(data);
+//
+//        // undo all highlights
+//        chart.highlightValues(null);
+//
+//        chart.invalidate();
+//    }
 
     private SpannableString generateCenterSpannableText() {
 
-        SpannableString s = new SpannableString("MPAndroidChart\ndeveloped by Philipp Jahoda");
+        SpannableString s = new SpannableString("Behavior Detection with Tensorflow");
         s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
         s.setSpan(new StyleSpan(Typeface.NORMAL), 14, s.length() - 15, 0);
         s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, s.length() - 15, 0);
